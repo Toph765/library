a8bd59c5ddd8fe97f912b8bfe0dceec995c377f1
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-09-21 15:37:12 +0800

    Add function to create and display books

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ function book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
     this.info = `Title: ${title}, Author: ${author}, Pages: ${pages}, Read status: ${readStatus} `
 }
+
+function addBookToLibrary(title, author, pages, readStatus) {
+    let newBook = new book(title, author, pages, readStatus);
+    myLibrary.push(newBook);
+}
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        display.textContent += myLibrary[i].info;
+    }
+}
+
+addBookToLibrary('a', 'b', 1, 'yes');
+addBookToLibrary('Percy Jackson', 'Rick Riordan', 500, "yes");
+addBookToLibrary('lord of the rings', 'jrr tolkien', 1500, 'no');
+displayBooks();
