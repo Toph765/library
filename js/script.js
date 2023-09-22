a2c183fafb02608ad2ecd82ca91103b446c77c11
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-09-22 17:10:57 +0800

    Add event listeners

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const main = document.querySelector("main");
 const addBook = document.querySelector(".add-book");
 const dialog = document.querySelector(".dialog");
 const inputDetails = document.querySelectorAll("input");
-const submitBtn = document.querySelector(".submitBtn")
+const submitBtn = document.querySelector(".submitBtn");
+
+// Object constructor
 
 function book(title, author, pages, readStatus) {
     this.title = title;
@@ -14,39 +16,54 @@ function book(title, author, pages, readStatus) {
     this.info = `Title: ${title}, Author: ${author}, Pages: ${pages}, Read status: ${readStatus} `
 }
 
+//
+
 function addBookToLibrary(title, author, pages, readStatus) {
     let newBook = new book(title, author, pages, readStatus);
     myLibrary.push(newBook);
 }
 
 function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        let card = document.createElement('div');
-        card.setAttribute("class", "card");
-        card.innerHTML = `
-            <div>${myLibrary[i].title}</div>
-            <div>${myLibrary[i].author}</div>
-            <div>${myLibrary[i].pages}</div>
-            <div>${myLibrary[i].readStatus}</div>
-            <button class="remove">Remove</button>`;
-        main.prepend(card);
-
-        /* display.textContent += myLibrary[i].info; */
-    }
+    
+    let card = document.createElement('div');
+    card.setAttribute("class", "card");
+    card.innerHTML = `
+        <div>${myLibrary[myLibrary.length - 1].title}</div>
+        <div>${myLibrary[myLibrary.length - 1].author}</div>
+        <div>${myLibrary[myLibrary.length - 1].pages}</div>
+        <div>${myLibrary[myLibrary.length - 1].readStatus}</div>
+        <button class="remove">Remove</button>`;
+    main.prepend(card);
 }
 
 addBook.addEventListener('click', () => {
     dialog.showModal();
 })
 
-inputDetails.forEach( input => input.addEventListener("change", (e) => {
-    submitBtn.value = input.value
+//inputs
+
+let title = "";
+let author = "";
+let pages = "";
+let readStatus = "";
+
+inputDetails.forEach(input => input.addEventListener("change", () => {
+    if (input.getAttribute("id") === "title") {title = input.value};
+    if (input.getAttribute("id") === "author") {author = input.value};
+    if (input.getAttribute("id") === "pages") {pages = input.value};
+    if (input.getAttribute("id") === "read-status") {readStatus = input.value};
+    console.log(title, author, pages, readStatus);
 }))
 
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBookToLibrary(title, author, pages, readStatus)
+    displayBooks();
+    dialog.close();
+})
 
 
-
+/* 
 addBookToLibrary('a', 'b', 1, 'yes');
 addBookToLibrary('Percy Jackson', 'Rick Riordan', 500, "yes");
-addBookToLibrary('lord of the rings', 'jrr tolkien', 1500, 'no');
-displayBooks();
+addBookToLibrary('lord of the rings', 'jrr tolkien', 1500, 'no'); */
