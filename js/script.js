ff4b2f9cdb20ea5f2cdba8640b5add88f7e90cac
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-09-26 14:31:31 +0800

    Add remove book functionality

--- js/script.js
+++ js/script.js
@@ -8,31 +8,33 @@ const submitBtn = document.querySelector(".submitBtn");
 
 // Object constructor
 
-function book(title, author, pages, readStatus) {
+function book(title, author, pages, readStatus, index) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
-    this.info = `Title: ${title}, Author: ${author}, Pages: ${pages}, Read status: ${readStatus} `
+    this.info = `Title: ${title}, Author: ${author}, Pages: ${pages}, Read status: ${readStatus} `;
+    this.index = `index${index}`;
 }
 
 //
 
-function addBookToLibrary(title, author, pages, readStatus) {
-    let newBook = new book(title, author, pages, readStatus);
+function addBookToLibrary(title, author, pages, readStatus, index) {
+    let newBook = new book(title, author, pages, readStatus, index);
     myLibrary.push(newBook);
 }
 
 function displayBooks() {
     
     let card = document.createElement('div');
-    card.setAttribute("class", "card");
+    card.setAttribute("class", "card ");
+    card.setAttribute("data-index", `${myLibrary[myLibrary.length-1].index}`)
     card.innerHTML = `
         <div>${myLibrary[myLibrary.length - 1].title}</div>
         <div>${myLibrary[myLibrary.length - 1].author}</div>
         <div>${myLibrary[myLibrary.length - 1].pages}</div>
         <div>${myLibrary[myLibrary.length - 1].readStatus}</div>
-        <button class="remove">Remove</button>`;
+        <button class="remove" data-index="${myLibrary[myLibrary.length-1].index}">Remove</button>`;
     main.prepend(card);
 }
 
@@ -46,6 +48,7 @@ let title = "";
 let author = "";
 let pages = "";
 let readStatus = "";
+let index = 0;
 
 inputDetails.forEach(input => input.addEventListener("change", () => {
     if (input.getAttribute("id") === "title") {title = input.value};
@@ -57,11 +60,31 @@ inputDetails.forEach(input => input.addEventListener("change", () => {
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    addBookToLibrary(title, author, pages, readStatus)
+    addBookToLibrary(title, author, pages, readStatus, index += 1)
     displayBooks();
     dialog.close();
+    console.log(myLibrary)
 })
 
+document.addEventListener("click", (e) => {
+    const target = e.target.closest(".remove");
+
+    if (target) {
+       let targetIndex = target.getAttribute("data-index");
+       const card = document.querySelectorAll(".card");
+
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (targetIndex === myLibrary[i].index) {
+                card.forEach(card => {
+                    if (targetIndex === card.getAttribute("data-index")) {
+                        card.remove();
+                    }
+                })
+            return myLibrary.splice(i, 1);
+            }
+        }
+    }
+})
 
 /* 
 addBookToLibrary('a', 'b', 1, 'yes');
