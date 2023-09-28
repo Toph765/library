55615fc0f839ffce0fbe6be6a9b3c8050264525b
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-09-28 17:13:46 +0800

    Dynamically change read status; refactor reset();

--- js/script.js
+++ js/script.js
@@ -33,16 +33,25 @@ function displayBooks() {
         <div>${myLibrary[myLibrary.length - 1].title}</div>
         <div>${myLibrary[myLibrary.length - 1].author}</div>
         <div>${myLibrary[myLibrary.length - 1].pages}</div>
-        <button>${myLibrary[myLibrary.length - 1].readStatus}</button>
+        <button class="is-read" data-index="${myLibrary[myLibrary.length-1].index}">${myLibrary[myLibrary.length - 1].readStatus}</button>
         <button class="remove" data-index="${myLibrary[myLibrary.length-1].index}">Remove</button>`;
     main.prepend(card);
 }
 
 function reset() {
     inputDetails.forEach( input => {
-        if (input.getAttribute("id") === "title") {input.value = ""};
-        if (input.getAttribute("id") === "author") {input.value = ""};
-        if (input.getAttribute("id") === "pages") {input.value = ""};
+        if (input.getAttribute("id") === "title") {
+            title = "";
+            input.value = "";
+        };
+        if (input.getAttribute("id") === "author") {
+            author = "";
+            input.value = "";
+        };
+        if (input.getAttribute("id") === "pages") {
+            pages = "";
+            input.value = "";
+        };
     })
 }
 
@@ -68,13 +77,35 @@ isRead.addEventListener("change", () => {
     readStatus = isRead.value;
 })
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     if (title && author && pages && readStatus) {
         addBookToLibrary(title, author, pages, readStatus, index += 1)
         displayBooks();
         reset();
         dialog.close();
-    } else {return};
+    }
+    
+})
+
+document.addEventListener("click", (e) => {
+    const target = e.target.closest(".is-read");
+
+    if (target) {
+        let targetIndex = target.getAttribute("data-index");
+
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (targetIndex === myLibrary[i].index) {
+                if (myLibrary[i].readStatus === "Finished") {
+                    myLibrary[i].readStatus = "Unfinished";
+                    target.textContent = myLibrary[i].readStatus;
+                } else {
+                    myLibrary[i].readStatus = "Finished";
+                    target.textContent = myLibrary[i].readStatus;
+                }
+            }
+        }
+    }
 })
 
 document.addEventListener("click", (e) => {
